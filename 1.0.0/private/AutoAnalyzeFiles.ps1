1292a69d6fce25208a8e8186cd0533249afc54de
Francesco Tili <francesco.tili@hotmail.it>
2022-12-03 18:19:27 +0100

    Added folder-wise analysis and normalization Analysis now proceed per folder and we have also two ProgressBar

--- 1.0.0/private/AutoAnalyzeFiles.ps1
+++ 1.0.0/private/AutoAnalyzeFiles.ps1
@@ -16,50 +16,67 @@ function AutoAnalyzeFiles {
 
   # Detect total number of files
   $fileNumber = 0
-  $folderList = Get-ChildItem -Path $workingFolder -Directory -Recurse -ErrorAction SilentlyContinue -Force
-  $folderList | ForEach-Object {
-    $fileList = Get-ChildItem -Path $_.FullName -File
-    $fileNumber += $fileList.Count
+  $folderListObj = Get-ChildItem -Path $workingFolder -Directory -Recurse -ErrorAction SilentlyContinue
+
+  # Create an array of folder paths
+  $folderList = [System.Collections.ArrayList]::new()
+  $folderListObj | ForEach-Object {
+    $folderList.Add($_.FullName) 1> $null
   }
-  
-  $fileList = Get-ChildItem -Path $workingFolder -File
+  $folderList.Add($workingFolder) 1> $null # Include also $working Folder
 
-  # Initialize progress bar variables
-  $fileCompleted = 0
-  $activity = "   ANALYZING FOLDER: $(($workingFolder.split("\"))[-1])"
-  $fileNumber = $fileList.Count
-  $fileProgressBar = [ProgressBar]::new($activity, 1, $fileNumber)
+  # Initialize folder progress bar variable
+  $folderCompleted = 0
+  $folderNumber = $folderList.Count
+  $folderActivity = "   WORKING ON YOUR MUSIC FILES"
+  $folderProgressBar = [ProgressBar]::new($folderActivity, 0, $folderNumber)
 
-  # Start looping on all the files of the workingFolder
-  $fileList | ForEach-Object {
-    # Variables for the file
-    $currentFile = @{
-      fullFilePath = $_.FullName
-      path         = Split-Path -Path $_.FullName -Parent
-      name         = (GetFilename( Split-Path -Path $_.FullName -Leaf )).fileName
-      extension    = (GetFilename( Split-Path -Path $_.FullName -Leaf )).extension
-    }
+  # Start looping on all the folders
+  foreach ($folderPath in $folderList) {
+    $folderCompleted += 1
+    $folderName = Split-Path -Path $folderPath -Leaf
+    $folderProgressBar.UpdateProgress("Analyzing folder: $($folderName)", $folderCompleted)
+    Write-Host " Now analyzing folder: $($folderName) " -Background DarkYellow -Foreground Black
 
-    $fileCompleted += 1
-    $fileProgressBar.UpdateProgress("Analyzing $($currentFile.name).$($currentFile.extension) ...", $fileCompleted)
-    Write-Host " $($fileCompleted)/$($fileNumber) | $($currentFile.name).$($currentFile.extension) " -Background Yellow -Foreground Black
+    $fileList = Get-ChildItem -Path $folderPath -File
 
-    if ( (CheckFileType $currentFile) -eq 'Supported' ) {
-      $maxVolume = Get-VolumeInfo $currentFile
-      if ($maxVolume -ne 0) {
-        OutputVolumeAnalysis "adjustmentNeeded" $maxVolume
-        NormalizeVolume $currentFile $maxVolume
+    # Initialize file progress bar variables
+    $fileCompleted = 0
+    $fileNumber = $fileList.Count
+    $fileActivity = " " # Leaved empty for output purpouse
+    $fileProgressBar = [PRogressBar]::new($fileActivity, 1, $fileNumber)
+    
+    # Start looping on all the files of the workingFolder
+    $fileList | ForEach-Object {
+      # Variables for the file
+      $currentFile = @{
+        fullFilePath = $_.FullName
+        path         = Split-Path -Path $_.FullName -Parent
+        name         = (GetFilename( Split-Path -Path $_.FullName -Leaf )).fileName
+        extension    = (GetFilename( Split-Path -Path $_.FullName -Leaf )).extension
+      }
+
+      $fileCompleted += 1
+      $fileProgressBar.UpdateProgress("Analyzing $($currentFile.name).$($currentFile.extension) ...", $fileCompleted)
+      Write-Host " $($fileCompleted)/$($fileNumber) | $($currentFile.name).$($currentFile.extension) " -Background Yellow -Foreground Black
+
+      if ( (CheckFileType $currentFile) -eq 'Supported' ) {
+        $maxVolume = Get-VolumeInfo $currentFile
+        if ($maxVolume -ne 0) {
+          OutputVolumeAnalysis "adjustmentNeeded" $maxVolume
+          NormalizeVolume $currentFile $maxVolume
+        }
+        else {
+          OutputVolumeAnalysis "noAdjustment"
+        }
       }
       else {
-        OutputVolumeAnalysis "noAdjustment"      
+        # File probably not supported
+        OutputCheckFileType "unsupported"
       }
-    }
-    else {
-      # File probably not supported
-      OutputCheckFileType "unsupported"
-    }
 
     
-    Write-Host ""
-  }#>
+      Write-Host ""
+    }
+  }
 }
