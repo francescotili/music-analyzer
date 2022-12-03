40e956bb42153c64741a9fb061ad83549e28bd9d
Francesco Tili <francesco.tili@hotmail.it>
2022-12-03 09:54:47 +0100

    Added function to parse FileName components

--- 1.0.0/private/Helpers.ps1
+++ 1.0.0/private/Helpers.ps1
@@ -100,3 +100,35 @@ function CalculateEtaOutput {
 
   return $output
 }
+
+function GetFilename {
+  <#
+    .SYNOPSIS
+      Extract the filename removing the extension
+    
+    .EXAMPLE
+      $response = GetFilename($filename);
+      $name = $response.name
+      $extension = $response.extension
+    
+    .PARAMETER File
+      Required. The complete name of the file with extension. Do not pass a relative or absolute path
+  #>
+
+  [CmdletBinding(DefaultParameterSetName)]
+  param (
+    [Parameter(Mandatory = $true)]
+    [String]$file
+  )
+  
+  $pattern = "(.+?)(\.[^.]*$|$)"
+
+  # Match the file with pattern
+  $regMatches = [regex]::Matches($file, $pattern)
+
+  # Return the filename and extension
+  return @{
+    fileName  = $regMatches.Groups[1].Value
+    extension = ($regMatches.Groups[2].Value).replace('.', '')
+  }
+}
