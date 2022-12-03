2ed6422133cab9597e6f8bb6b3449eb2a421159d
Francesco Tili <francesco.tili@hotmail.it>
2022-12-03 09:52:29 +0100

    Implemented Progress bar and ETA calculations

--- 1.0.0/private/AutoAnalyzeFiles.ps1
+++ 1.0.0/private/AutoAnalyzeFiles.ps1
@@ -13,8 +13,27 @@ function AutoAnalyzeFiles {
     [String]
     $workingFolder
   )
-  
-  Write-Host "Working main function"
-  Write-Host "Folder passed:"
-  Write-Host $workingFolder
+
+  $fileList = Get-ChildItem -Path $workingFolder -File
+
+  # Initialize progress bar variables
+  $fileCompleted = 0
+  $progress = 0
+  $activity = "   ANALYZING FOLDER: $(($workingFolder.split("\"))[-1])"
+  $fileNumber = $fileList.Count
+  $etaStartTime = Get-Date
+
+  # Start looping on all the files of the workingFolder
+  $fileList | ForEach-Object {
+    # Initialize progress bar
+    $etaOutput = CalculateEtaOutput $etaStartTime $fileCompleted $fileNumber
+    $progress = 100 * (($fileCompleted) / ($fileNumber))
+    $barStatus = "{0:N1}% - Time remaining: {1}" -f $progress, $etaOutput
+    $status = "{0:N1}%" -f $progress
+
+    $fileCompleted += 1
+    Write-Progress -Activity $activity -PercentComplete $progress -CurrentOperation "Analyzing $($currentFile.name).$($currentFile.extension) ..." -Status "$($barStatus)"
+    Write-Host " $($_.FullName) | $($status) " -Background Yellow -Foreground Black
+    Write-Host ""
+  }
 }
--- 1.0.0/private/Helpers.ps1
+++ 1.0.0/private/Helpers.ps1
@@ -22,5 +22,81 @@ function Set-Path {
     # No path specified
     OutputUserError "emptyPath"
   }
+}
 
+function CalculateEtaOutput {
+  <#
+    .SYNOPSIS
+      This functions calculate estimated remaining time to complete an operation
+    
+    .PARAMETER startTime
+      The start time of the operation
+    
+    .PARAMETER itemCompleted
+      Number of completed single items
+    
+    .PARAMETER itemTotal
+      Total number of items to be elaborated
+   #>
+
+  [CmdletBinding(DefaultParameterSetName)]
+  param (
+    [Parameter(Mandatory = $true)]
+    [DateTime]
+    $startTime,
+       
+    [Parameter(Mandatory = $true)]
+    [Int32]
+    $itemCompleted,
+       
+    [Parameter(Mandatory = $true)]
+    [Int32]
+    $itemTotal
+  )
+
+  $output = ""
+  $now = Get-Date
+
+  if ( $totalCompleted -gt 0) {
+    # Calculate remaining time
+    $elapsed = $now - $startTime
+    $speed = $elapsed.TotalSeconds / $itemCompleted
+    $timeLeft = New-TimeSpan -Seconds (($itemTotal - $itemCompleted) * $speed)
+
+    # Build output string
+    if ($timeLeft.Days -gt 0) {
+      if ($timeLeft.Days -gt 1) {
+        $output += "$($timeLeft.Days) days "
+      }
+      else {
+        $output += "$($timeLeft.Days) day"
+      }
+    }
+    if ($timeLeft.Hours -gt 0) {
+      if ($timeLeft.Hours -gt 1) {
+        $output += "$($timeLeft.Hours) hours "
+      }
+      else {
+        $output += "$($timeLeft.Hours) hour"
+      }
+    }
+    if ($timeLeft.Minutes -gt 0) {
+      if ($timeLeft.Minutes -gt 1) {
+        $output += "$($timeLeft.Minutes) minutes "
+      }
+      else {
+        $output += "$($timeLeft.Minutes) minute"
+      }
+    }
+    if ($timeLeft.Seconds -gt 0) {
+      if ($timeLeft.Seconds -gt 1) {
+        $output += "$($timeLeft.Seconds) seconds "
+      }
+      else {
+        $output += "$($timeLeft.Seconds) second"
+      }
+    }
+  }
+
+  return $output
 }
