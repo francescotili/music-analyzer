d9088603b3ca27aee9a3b285c5c898d499456a84
Francesco Tili <francesco.tili@hotmail.it>
2022-12-04 11:05:34 +0100

    Output enhancements

--- 1.0.0/private/Outputs.ps1
+++ 1.0.0/private/Outputs.ps1
@@ -123,11 +123,6 @@ function OutputScriptFooter {
   Write-Host "                               " -BackgroundColor DarkGreen -ForegroundColor White
 
   (New-Object System.Media.SoundPlayer "$env:windir\Media\Alarm03.wav").Play()
-  for ( $i = 0; $i -lt $completed.Length; $i++ ) {
-    Write-Host $completed[$i] -NoNewline
-  }
-  Write-Host ""
-  Write-Host ""
   Write-Host ""
 }
 
