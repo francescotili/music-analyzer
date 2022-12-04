922b0240b0dd1773793dba435aff2d0004d8deb6
Francesco Tili <francesco.tili@hotmail.it>
2022-12-04 10:10:19 +0100

    Control if workingFolder is set

--- 1.0.0/private/CleanBackups.ps1
+++ 1.0.0/private/CleanBackups.ps1
@@ -7,5 +7,20 @@ function CleanBackups {
       Optional. The folder to scan and to clean. By default it uses the passed path, if present, otherwise it will ask the user.
   #>
 
-  Write-Host "It works!"
+  [CmdletBinding(DefaultParameterSetName)]
+  param (
+    [Parameter(Mandatory = $false)]
+    [String]
+    $workingFolder
+  )
+
+  # Analyze and set the workingFolder
+  if ($workingFolder -ne "") {
+    # Global working folder is set - i.E. after normal workflow of analyzing
+    Write-Host "Found a global working folder"
+  }
+  else {
+    # No global specified, ask the user
+    Write-Host "Ask the user"
+  }
 }
